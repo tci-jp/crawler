df313e06e619195e95f83ce39dd8c7587b0cc3e3
Nguyen Vu Quoc Tu <tu.nvq@dectech.global>
2017-08-28 07:26:43 +0000

    Merged PR 286: [Add] Test function TestCustomParsing
    [Add] Test function TestCustomParsing
    
    Related work items: #578

--- CrawlerLib/CrawlerLib.Tests/CrawlerTests.cs
+++ CrawlerLib/CrawlerLib.Tests/CrawlerTests.cs
@@ -6,10 +6,12 @@ namespace CrawlerLib.Tests
 {
     using System;
     using System.Collections.Async;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using Azure;
+    using CrawlerLib.Metadata;
     using Data;
     using FluentAssertions;
     using global::Azure.Storage;
@@ -18,7 +20,7 @@ namespace CrawlerLib.Tests
     using Xunit;
     using Xunit.Abstractions;
 
-    public class TestCrawler
+    public class CrawlerTests
     {
         private const string Owner = "unittest";
         private readonly Configuration crawlerConfig = new Configuration();
@@ -26,7 +28,7 @@ namespace CrawlerLib.Tests
 
         private Crawler crawler;
 
-        public TestCrawler(ITestOutputHelper output)
+        public CrawlerTests(ITestOutputHelper output)
         {
             this.output = output;
 
@@ -109,5 +111,47 @@ namespace CrawlerLib.Tests
             var str = new StreamReader(stream).ReadToEnd();
             str.Should().Contain("<body").And.Contain("</body>");
         }
+
+        [Theory]
+        [InlineData("fn:match(string(//div/@id),'(\\d+)')", "http://www.dectech.tokyo/", new[]
+                                                      {
+                                                         "02"
+                                                      })]
+        [InlineData("fn:match(string(//input/@id), '(\\D+)')", "http://www.dectech.tokyo/", new[]
+                                                      {
+                                                         "q"
+                                                      })]
+        [InlineData("fn:match(string(//h1/@class), '(\\D+\\d+)')", "http://www.dectech.tokyo/", new[]
+                                                      {
+                                                         "display-4"
+                                                      })]
+        public async Task TestCustomParsing(string xpath, string url, string[] result)
+        {
+            crawlerConfig.HostDepth = 0;
+            crawlerConfig.Depth = 0;
+            var extractor = new XPathMetadataExtractor(new[] { new KeyValuePair<string, string>(xpath, "field") });
+            crawlerConfig.MetadataExtractors = new XPathMetadataExtractor[] { extractor };
+
+            var session = await Crawler.InciteStart(Owner, new[] { new Uri(url) });
+
+            var sesspage = await crawlerConfig.Storage.GetSessions(Owner, new[] { session.SessionId });
+            sesspage.Items.Count().Should().Be(1);
+            var sessinfo = sesspage.Items.Single();
+            sessinfo.State.Should().Be(SessionState.InProcess);
+
+            await session.CrawlerTask;
+
+            sesspage = await crawlerConfig.Storage.GetSessions(Owner, new[] { session.SessionId });
+            sesspage.Items.Count().Should().Be(1);
+            sessinfo = sesspage.Items.Single();
+            sessinfo.State.Should().Be(SessionState.Done);
+
+            var urls = await crawlerConfig.Storage.GetSessionUris(session.SessionId).ToListAsync();
+            urls.Select(u => u.Uri.ToString()).Should().BeEquivalentTo(url);
+            var resultMetadata = await crawlerConfig.Storage.GetUriMetadata(Owner, url).ToListAsync();
+            resultMetadata.Select(t => t.Value.ToString()).Should().BeEquivalentTo(result);
+            resultMetadata.Select(t => t.Key).Should().BeEquivalentTo(new[] { "field" });
+        }
     }
+
 }

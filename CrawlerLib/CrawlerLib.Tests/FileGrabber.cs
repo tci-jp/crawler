119fe30f6488fe5489116a7171cd943d1dd3ef3a
Shankar <shankar@dectech.global>
2017-08-29 09:13:17 +0000

    Merged PR 301: HttpGrabber using files for unittesting
    HttpGrabber using files for unittesting
    
    Related work items: #565

---
+++ CrawlerLib/CrawlerLib.Tests/FileGrabber.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="FileGrabber.cs" company="DECTech.Tokyo">
+// Copyright (c) DECTech.Tokyo. All rights reserved.
+// </copyright>
+namespace CrawlerLib.Tests
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Threading.Tasks;
+    using CrawlerLib.Grabbers;
+
+    public class FileGrabber : HttpGrabber
+    {
+        public FileGrabber(Configuration config)
+            : base(config)
+        {
+        }
+
+        /// <inheritdoc />
+        public override async Task<GrabResult> Grab(Uri uri, Uri referer)
+        {
+            await Task.Yield();
+            var uriPath = uri.LocalPath;
+            var filePath = Directory.GetCurrentDirectory() + uriPath;
+            GrabResult grabResult = null;
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath);
+                grabResult = new GrabResult { Status = HttpStatusCode.OK, Content = content };
+            }
+            else
+            {
+                grabResult = new GrabResult { Status = HttpStatusCode.NotFound, Content = null };
+            }
+
+            return grabResult;
+        }
+    }
+}
---
+++ CrawlerLib/CrawlerLib.Tests/FileGrabberTest.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="FileGrabberTest.cs" company="DECTech.Tokyo">
+// Copyright (c) DECTech.Tokyo. All rights reserved.
+// </copyright>
+namespace CrawlerLib.Tests
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class FileGrabberTest
+    {
+        [Fact]
+        public async Task SuccessCheck()
+        {
+            Configuration config = new Configuration();
+            var grab = new FileGrabber(config);
+            var res = await grab.Grab(new Uri("//FileGrabbersample.txt"), null);
+            Assert.Equal(expected: "TestContent", actual: res.Content);
+            Assert.Equal(expected: HttpStatusCode.OK, actual: res.Status);
+        }
+
+        [Fact]
+        public async Task FailureCheck()
+        {
+            Configuration config = new Configuration();
+            var grab = new FileGrabber(config);
+            var res = await grab.Grab(new Uri("//FileGrabberabcd.txtt"), null);
+            Assert.Equal(expected: null, actual: res.Content);
+            Assert.Equal(expected: HttpStatusCode.NotFound, actual: res.Status);
+        }
+    }
+}
